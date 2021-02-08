786489b3a3265c95be729501aa7c7798c35195e6
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-08 15:37:01 -0800

    add ByCharacter used hooks

--- src/containers/ByCharacter.jsx
+++ src/containers/ByCharacter.jsx
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 import CharacterList from '../components/list/CharacterList';
-import { fetchCharacters } from '../services/xfilesAPI';
+import { fetchCharacters } from '../services/heyArnoldAPI';
 
-export default class ByCharacter extends Component {
-  render() {
-    return <div></div>;
-  }
+function ByCharacter() {
+  const [character, setCharacter] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchCharacters().then((res) => {
+      setCharacter(res), setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
+  return (
+    <div>
+      <CharacterList character={character} />
+    </div>
+  );
 }
+
+export default ByCharacter;
