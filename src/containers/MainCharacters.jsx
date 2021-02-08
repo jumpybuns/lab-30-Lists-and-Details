4a7642f00aadef1a5f4bb5adcaecf3c58c724be2
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-08 14:14:38 -0800

    add MainCharacters.jsx

---
+++ src/containers/MainCharacters.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import CharacterList from '../components/list/CharacterList';
+import PropTypes from 'prop-types';
+import { fetchCharacters } from '../services/xfilesAPI';
+
+export default class MainCharacters extends Component {
+  state = {
+    loading: true,
+    characters: [],
+  };
+
+  async componentDidMount() {
+    const characters = await fetchCharacters();
+    this.setState({
+      loading: false,
+      characters,
+    });
+  }
+  render() {
+    const { loading, characters } = this.state;
+
+    if (loading) return <h1>Loading...</h1>;
+    return (
+      <div>
+        <CharacterList characters={characters} />
+      </div>
+    );
+  }
+}
