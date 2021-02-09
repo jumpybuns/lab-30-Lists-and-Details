95d75601e7a4bbd5f9a2dfcbcd9464fba0fa1f94
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-08 16:25:16 -0800

    add Character css

--- src/components/list/Character.jsx
+++ src/components/list/Character.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styles from './Character.css';
 
 const Character = ({ name, image }) => (
   <Link to={`/details/${name}`}>
     <figure>
-      <img src={image} alt={name} />
+      <img className={styles.image} src={image} alt={name} />
       <figcaption>
-        <p>{name}</p>
+        <p className={styles.name}>{name}</p>
       </figcaption>
     </figure>
   </Link>
